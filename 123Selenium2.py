41a6e6b598ca1c4578b26c1d3cbbb788e67d82a9
Balaozinho <ogrupogali@gmail.com>
2026-01-04 16:14:58 -0300

    Primeiro Commit

---
+++ 123Selenium2.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.com")
+
+campo_pesquisa = driver.find_element(By.NAME, "q")
+campo_pesquisa.send_keys("Python Selenium")
+
+input("Pressione ENTER para fechar o navegador...")
+driver.quit()
